650ddc03cc4a1a7f23f348fca6c199d3b7122518
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-08-08 23:43:28 -0400

    Adding needed LSPs

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -672,18 +672,43 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
 
+        -- Go
+        gopls = {},
+
+        -- Python
+        pyright = {},
+        ruff = {},
+
+        -- Rust
+        rust_analyzer = {},
+
+        -- Web
+        cssls = {},
+        eslint = {},
+        ts_ls = {},
+        svelte = {},
+        vue_ls = {},
+        html = {},
+        prettier = {},
+
+        -- CI/CD
+        gh_actions_ls = {},
+        docker_compose_language_service = {},
+        docker_language_server = {},
+
+        -- Config Files
+        jsonls = {},
+        yamlls = {},
+
+        -- Game Dev
+        -- gdscript = {},
+        -- gdshader_ls = {},
+
+        -- DBs
+        postgres_lsp = {},
+
+        -- Lua
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
