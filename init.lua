6cbda391bac7c7165dbd55dc5783baa3f57a1133
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-08-27 22:04:42 -0400

    Changing default tab width

--- init.lua
+++ init.lua
@@ -166,6 +166,10 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- set default tab size
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
